da48f1f39daf4beef544e0d56035c8dcc9b8380b
Keynn2555 <75205651+Keynn2555@users.noreply.github.com>
2024-03-27 19:01:04 +0800

    chore: updated service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cfv72nd1n4"
+    "revision": "0.b5ifdks42m"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
